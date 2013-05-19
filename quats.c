05da3a87ecaa67dea7354686fd89e4a9551f0275
quaeldurn <quaeldurn@gmail.com>
2013-05-19 07:56:01 +0000

    modified quats.c

--- quats.c
+++ quats.c
@@ -2,8 +2,10 @@
 
 int main() 
 {
+	int n = 0;
 	long int i;
-	for (i=1; i < 120000; i *= 4) {
-		printf("0\t%d\t%d\t%d\n", i, i*2, i*3);
+	for (i=1; i < 12000000; i *= 4) {
+		printf("%2d :: 0\t%lu\t%lu\t%lu\n", n, i, i*2, i*3);
+		n++;
 	}
 }
