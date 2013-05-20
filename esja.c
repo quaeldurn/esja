e3c347f093b88532bed75ef5df6cf390eec6035c
quaeldurn <quaeldurn@gmail.com>
2013-05-20 19:48:25 +0000

    made *_qit prettier and did other fixes

--- esja.c
+++ esja.c
@@ -13,49 +13,91 @@
 
 int read_bit();
 int read_qit();
-int terminal();
-int cu();
+unsigned int terminal();
+int exec(unsigned int location)
 
 int mov();
 int add();
 
-typedef unsigned char word; // default word size = 8 bits
+typedef uint16_t word;	// default word, = 8 qits
 word r[35];	// defining registers
-char **cmnd = NULL, status = 'a';
+char status = 'a';
 
 int main()
 { 	
-	cmnd = malloc(47);
 	void *mem = malloc(sizeof(word) * MEM_SIZE);	// mem allocation
-	int quit_loop;
+	char quit_loop;
 	while (quit_loop) {
-		terminal(); //gets a location of a block to execute
-		control_unit();  //executes the block
+		terminal();	//gets a location of a block to execute
+		exec(); 	//executes the block
 	}
 }
 
-int read_bit (word *c, int n)
+int read_bit(word *c, int n)
 {
 	static unsigned char mask[] = {128, 64, 32, 16, 8, 4, 2, 1};
 	return ((*c & mask[n]) != 0);
 }
 
-int read_qit (word *c, int n) // reads qit n in byte c
+int read_qit(unsigned int location, unsigned char n)	// reads qit n in byte c
 {
 	int i;
-	static word mask[] = {128, 64, 32, 16, 8, 4, 2, 1};
-	if (n > 3) {
-		printf("read_qit invalid index (n)"); 
-		return 0;
+	if (n > 8) {
+		printf("read_qit invalid index");	// better error returnment?
+		return 9;
 	}
-	if ((*c & mask[n*2]) == 0 && (*c & mask[n*2+1]) == 0) {
-		return (0);
-	} else if ((*c & mask[n*2+1]) == 0 && (*c & mask[n*2]) == 1) {
-		return (1);	
-	} else if ((*c & mask[n*2]) == 1 && (*c & mask[n*2+1]) == 0) {
-		return (2);
-	} else if ((*c & mask[n*2]) == 1 && (*c & mask[n*2+1]) == 1) {
-		return (3);
+	if (((mem[location] >> n*2) & 1) == 0 && ((mem[location] >> n*2+1) & 1) == 0) {
+		return 0;
+	} else if (((mem[location] >> n*2) & 1) == 0 && ((mem[location] >> n*2+1) & 1) == 1) {
+		return 1;	
+	} else if (((mem[location] >> n*2) & 1) == 1 && ((mem[location] >> n*2+1) & 1) == 0) {
+		return 2;
+	} else if (((mem[location] >> n*2) & 1) == 1 && ((mem[location] >> n*2+1) & 1) == 1) {
+		return 3;
+	}
+}
+
+int write_qit(unsigned int location, char n) 
+{
+	if (n > 8) {
+		printf("write_qit invalid index");	// need a better error thingy
+	}
+
+	if (n == 0) {
+		mem[location] &= ~(1 << n*2);	// set to 0
+		mem[location] &= ~(1 << n*2+1);	// set to 0
+	} else if (n == 1) {
+		mem[location] &= ~(1 << n*2);	// set to 0
+		mem[location] |= (1 << n*2+1);	// set to 1
+	} else if (n == 2) {
+		mem[location] |= (1 << n*2);
+		mem[location] &= ~(1 << n*2+1);
+	} else if (n == 3) {
+		mem[location] |= (1 << n*2);
+		mem[location] |= (1 << n*2+1)
+	}
+}
+
+unsigned int terminal()
+{
+	char str[50], *result, n = 0;
+	unsigned int location;
+
+	if (status == 'a') {
+		printf("number of available qits: %d\n", MEM_SIZE*4);
+		printf("Umriturnarrettur: Oll rettindi fraskilin\n");
+		status = '0';
+	}
+	printf(">");
+	fgets(str, 47, stdin);
+	sscanf(str, "%u", location);
+	return location;	// return location to a bw (base word)
+}
+
+int exec(int location)	// controle unit
+{
+	if (word) {
+		
 	}
 }
 
@@ -68,30 +110,3 @@ int add(int n, word r2)
 {
 	r[n] = r[n] + r2;
 }
-
-int cu() // controle unit
-{
-;
-}
-
-int terminal()
-{
-	char str[50], *result, n = 0;
-	long int location;
-
-	if (status == 'a') {
-		printf("number of available qits: %d\n", MEM_SIZE*4);
-		printf("Umriturnarrettur: Oll rettindi fraskilin\n");
-		status = '0';
-	}
-	printf(">");
-	fgets(str, 47, stdin);
-	sscanf(str, "%lu", &location);
-	result = strtok(str, " ");
-	while (n < 4 && result != NULL) {
-		cmnd[n] = result;
-		result = strtok(NULL, " ");
-		n++;
-	}
-}
-
