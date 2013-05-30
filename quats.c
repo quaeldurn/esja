f07691397f1df54006de42f13df16e2e61d1aafd
quaeldurn <quaeldurn@gmail.com>
2013-05-30 09:01:45 +0000

    Update quats.c

--- quats.c
+++ quats.c
@@ -2,10 +2,10 @@
 
 int main() 
 {
-	int n = 0;
-	long int i;
-	for (i=1; i < 12000000; i *= 4) {
-		printf("%2d :: 0\t%lu\t%lu\t%lu\n", n, i, i*2, i*3);
+	unsigned int n = 0;
+	unsigned long long int i;
+	for (i; i < 12000000; i *= 4) {
+		printf("%2d :: 0\t%llu\t%llu\t%llu\n", n, i, i*2, i*3);
 		n++;
 	}
 }
